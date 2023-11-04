19891d7f179276ae666ff7c907c772f7bfab8696
Andrei <albu.cosminandrei.1999@gmail.com>
2023-11-04 17:19:15 +0100

    changes

--- index.js
+++ index.js
@@ -94,18 +94,18 @@ function updateProducts() {
                     <div class="product-main-info" style="flex: 0 0 25%">
                         <img onclick="window.location.href = '/product/product.html?id=${elem._id}'" style="cursor:pointer" id="product-image" src="${elem.imageUrl}"/>
                         <div class="product-title-brand">
-                            <h3 style="margin: 0;">${elem.name}</h3>
+                            <h3 style="margin: 0;text-align:start;font-size: small"><b>${elem.name}</b></h3>
                             <p>${elem.brand}</p>
                         </div>
                     </div>
                     <div class="product-description" style="flex: 0 0 25%;">${elem.description}</div>
-                    <div class="product-price">$${elem.price}</div>
+                    <div class="product-price" style=font-size:small"">$${elem.price}</div>
                     <div class="modify-delete-product">
-                    <svg onclick="setUpdateProduct('${elem._id}')" data-bs-toggle="modal" data-bs-target="#updateProductModal" style="margin-right: 0.5rem;cursor:pointer;" xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#FC9D03" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                    <svg id="update-pencil" onclick="setUpdateProduct('${elem._id}')" data-bs-toggle="modal" data-bs-target="#updateProductModal" style="margin-right: 0.5rem;cursor:pointer;" xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#FC9D03" class="bi bi-pencil-square" viewBox="0 0 16 16">
                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                     </svg>
-                    <svg onclick="removeProduct('${elem._id}')" style="margin-left: 0.5rem;cursor:pointer;" xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#f70a0a" class="bi bi-trash" viewBox="0 0 16 16">
+                    <svg id="delete-pencil" onclick="removeProduct('${elem._id}')" style="margin-left: 0.5rem;cursor:pointer;" xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#f70a0a" class="bi bi-trash" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
                     </svg>
